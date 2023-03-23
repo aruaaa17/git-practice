7313acc87b7a42533d13d391e63eb140ed6c962b
Julie Loh <jlohusky@gmail.com>
2023-03-23 14:23:53 -0700

    Revert "resolved conflict"
    This reverts commit e425cc54e8aad09e3def5f601d0098572890f308, reversing
    changes made to 7602310c1ca46f09ff7bd7a513d930863407256c.

--- function_d.py
+++ function_d.py
@@ -2,9 +2,12 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    maximal = max(numbers)
-    return maximal 
-# UPDATE
+    max_value = 0
+    for num in numbers:
+        if num > max_value:
+            max_value = num
+    
+    return max_value
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
